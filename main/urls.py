b1a50a78a3ca4189f6f18d380579a8959162d150
abbaasalif <abbaasalif@yahoo.com>
2024-10-15 16:09:55 -0400

    no views for thankyous so not in urls.py

--- main/urls.py
+++ main/urls.py
@@ -7,6 +7,6 @@ urlpatterns = [
     path('register/', views.register, name='register'),
     path('program/', views.program, name='program'),
     path('waitlist/', views.waitlist, name='waitlist'), 
-    path('thank_you/', views.thank_you, name='thank_you'),
-    path('thank_you_waitlist/', views.thank_you_waitlist, name='thank_you_waitlist'),
+    # path('thank_you/', views.thank_you, name='thank_you'),
+    # path('thank_you_waitlist/', views.thank_you_waitlist, name='thank_you_waitlist'),
 ]
